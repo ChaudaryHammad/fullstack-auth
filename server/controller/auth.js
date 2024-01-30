f844f6b6b69bb7f54597cc8bc1a4efb07e2a338c
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-01-30 13:46:51 +0500

    sigin in done

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,16 +1,16 @@
 const User = require("../model/user");
 const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utils/error");
-const signup = async (req, res,next) => {
+
+const jwt = require("jsonwebtoken");
+const signup = async (req, res, next) => {
   const { userName, email, password } = req.body;
-  if(!userName || !email || !password){
-      return res.status(400).json({message:'please fill all the fields'})
+  if (!userName || !email || !password) {
+    return res.status(400).json({ message: "please fill all the fields" });
   }
 
   const hashPassword = bcryptjs.hashSync(password, 10);
 
-  
-  
   const newUser = new User({
     userName,
     email,
@@ -19,41 +19,48 @@ const signup = async (req, res,next) => {
 
   try {
     await newUser.save();
-    res.status(201).json({data:newUser,message:'user created successfully'});
+    res
+      .status(201)
+      .json({ data: newUser, message: "user created successfully" });
   } catch (error) {
     next(error);
   }
 };
 
-
-
-const signin = async(req,res,next)=>{
-  const {email,password} = req.body;
-  if(!email || !password){
-    return next(errorHandler(400,'please fill all the fields')
-    )
-
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(errorHandler(400, "please fill all the fields"));
   }
 
-    try {
-      const validUser = await User.findOne({email});
-      if(!validUser){
-        return next(errorHandler(400,'invalid email or password'))
-      }
-      const validPassword = bcryptjs.compareSync(password,validUser.password);
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "invalid email or password"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
 
-      if(!validPassword){
-        return next(errorHandler(400,'wrong credentials'))
-      }
-
-      
-
-
-    } catch (error) {
-      next(error)
+    if (!validPassword) {
+      return next(errorHandler(400, "wrong credentials"));
     }
 
+    const token  = jwt.sign({
+      id:validUser._id,
 
-}
+    },process.env.JWT_SECRET,{
+      expiresIn:"1d"
+    })
 
-module.exports = { signup ,signin};
+    const {password:hashedPassword,...rest} = validUser._doc
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+    res.cookie("access_token",token,{
+      httpOnly:true,
+     expires:expiryDate
+
+    }).status(200).json(rest)
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin };
