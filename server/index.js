203deb8763d3ea28151015c540afeee607696757
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-02-01 16:42:25 +0500

    addedd preproduction changes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+
+const path = require("path");
+require("dotenv").config();
+
+
 const app = express();
-require("dotenv").config({
-  path: "./config/.env",
+app.use(express.static(path.join(__dirname, "/client/dist/")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 const cookieParser = require("cookie-parser");
-
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const connectDB = require("./database/connection.js");
@@ -21,18 +27,16 @@ app.use(cookieParser());
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
-
-
 //custome middlewares
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        success:false,
-        message,
-        statusCode});
-})
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 
 //server
 app.listen(process.env.PORT, () => {
