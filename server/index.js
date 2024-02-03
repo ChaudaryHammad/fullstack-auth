8ef076a4f7a1818f775904ccb68fb8cb4b0db00b
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-02-03 18:22:31 +0500

    production setup done

--- server/index.js
+++ server/index.js
@@ -6,17 +6,23 @@ const path = require("path");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const connectDB = require("./database/connection.js");
-//connect to DB
+
+// Connect to DB
 connectDB();
 
-//middlewars
-
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
-app.use(express.static(path.join(__dirname, "./client/dist")))
+// API routes
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+// Wildcard route for serving React app
 app.get("*", (req, res) => {
   res.sendFile(
     path.join(__dirname, "./client/dist/index.html"),
@@ -28,25 +34,18 @@ app.get("*", (req, res) => {
   );
 });
 
+// Custom middleware for error handling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 
-//api routes
-app.use("/api/user", userRoute);
-app.use("/api/auth", authRoute);
-
-
-
-//custome middlewares
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        success:false,
-        message,
-        statusCode});
-})
-
-
-//server
+// Server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
