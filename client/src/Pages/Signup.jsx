13d016d5cb2302c802eab5a828a815c3e5ec14d7
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-02-03 18:08:19 +0500

    production setup done

--- client/src/Pages/Signup.jsx
+++
@@ -1,9 +0,0 @@
-import SignUp from "../components/SignUp"
-
-function Signup() {
-  return (
-    <div className=" h-[91vh] " style={{backgroundImage:'url("https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")'}}><SignUp/></div>
-  )
-}
-
-export default Signup
